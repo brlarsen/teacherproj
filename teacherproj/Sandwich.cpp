95907c25c7708feba1d9a945c1e94aff49305b75
brlarsen <cody.henrichsen@740s-pl-124.local>
2016-07-26 12:52:58 -0600

    Added sandwich stuff.

--- teacherproj/Sandwich.cpp
+++ teacherproj/Sandwich.cpp
@@ -7,3 +7,30 @@
 //
 
 #include <stdio.h>
+#include "Sandwich.h"
+
+//Constructor
+
+
+Sandwich :: Sandwich()
+{
+    
+    this->breadCount = 2;
+    this->filling = "peanut butter and sriracha";
+    this->name = "Spicy PB";
+
+}
+
+//end Constructors
+
+int Sandwich :: getBreadCount()
+{
+    
+    return breadCount;
+    
+}
+
+void Sandwich :: setBreadCount(int breadCount)
+{
+    this->breadCount = breadCount;
+}
