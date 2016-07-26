3d4cc9ca1312b3b5fa54cbf93bfa5547b7783433
brlarsen <cody.henrichsen@740s-pl-124.local>
2016-07-26 12:08:36 -0600

    Added some more stuff

--- teacherproj/main.cpp
+++ teacherproj/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+
+int main()
+{
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!" << endl;
+    double myFraction;
+    cout << myFraction << endl;
+    myFraction = 3.15;
+    cout << myFraction << endl;
+    int myFavoriteNumber;
+    cout << myFavoriteNumber << endl;
+    cin >> myFavoriteNumber;
+    cout << myFavoriteNumber << endl;
+    bool myBoolean = true;
+    cout << myBoolean;
+    string myWords;
+    myWords = "What's up bruh!";
+    cout << myWords << endl;
+    
     return 0;
 }
