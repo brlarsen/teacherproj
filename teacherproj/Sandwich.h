404464f9069b5ac5fb89bbdef6dd541632880768
brlarsen <cody.henrichsen@740s-pl-124.local>
2016-07-26 12:36:56 -0600

    Added header information to project.

---
+++ teacherproj/Sandwich.h
@@ -0,0 +1,38 @@
+//
+//  Sandwich.h
+//  teacherproj
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 Henrichsen, Cody. All rights reserved.
+//
+
+#ifndef Sandwich_h
+#define Sandwich_h
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class Sandwich
+{
+private:
+    int breadCount;
+    string filling;
+    string name;
+
+    
+public:
+    Sandwich();
+    void setBreadCount(int breadCount);
+    int getBreadCount();
+    void setFilling(string filling);
+    string getFilling();
+    string getName();
+    void SetName(string name);
+
+};
+
+
+
+#endif /* Sandwich_h */
