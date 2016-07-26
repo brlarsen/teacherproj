281f0b0dfba70f84d4c50f7868fec19247b12277
Henrichsen, Cody <cody.henrichsen@740s-pl-124.local>
2016-07-26 10:09:50 -0600

    Initial Commit

---
+++ teacherproj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  teacherproj
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 Henrichsen, Cody. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
